f2f582bbc2f8133a48b0c8649f4834534c30bc58
Thomas Vigouroux <tomvig38@gmail.com>
2020-04-30 14:17:51 +0200

    fuzzyhighlights

--- config.def.h
+++ config.def.h
@@ -5,13 +5,15 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=20"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#f8f8f2", "#282a36" },
 	[SchemeSel] = { "#282a36", "#bd93f9" },
+	[SchemeSelHighlight] = { "#ff5555", "#bd93f9" },
+	[SchemeNormHighlight] = { "#ff5555", "#282a36" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
